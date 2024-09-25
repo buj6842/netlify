8d956e89b6ce60e2c5a6899a94a2aa1ebc5d4933
parrot <buj6842@naver.com>
2024-09-25 18:49:00 +0900

    전체조정

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,31 +1,39 @@
-"use client";  // 이 파일이 클라이언트 컴포넌트임을 명시 << 현재
+"use client"; // 이 파일이 클라이언트 컴포넌트임을 명시 << 현재
 // back 단과 연동되는 부분이 없기에 컴포넌트를 서버형식으로 사용하기 위해서는 모든 페이지에 작성을 해주어야 컴포넌트로 사용가능함
 
 import React, { useEffect, useState } from "react";
 
-
 const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white py-6">
-            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-                <div className="text-center md:text-left">
-                    <h6 className="uppercase font-bold">KTDS</h6>
-                    <p className="mt-2">Copyright@ 2024 KTDS Corp. All Rights Reserved.</p>
-                </div>
-                <div className="flex space-x-4 mt-4 md:mt-0">
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        개인정보 처리방침
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        이용약관
-                    </a>
-                    <a href="#" className="text-gray-400 hover:text-white">
-                        Contact Us
-                    </a>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="bg-gray-800 text-white py-12">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <div className="text-center md:text-left">
+          <h6 className="uppercase">
+            <span className="footer">
+              ㈜케이티 경기도 성남시 분당구 불정로 90 (정자동)
+            </span>
+            <span className="footer">대표자명 : 김영섭</span>
+            <span className="footer">사업자등록번호 : 102-81-42945</span>
+            <span className="footer">통신판매업신고 : 2002-경기성남-0048</span>
+          </h6>
+          <p className="mt-2">
+            Copyright@ 2024 KTDS Corp. All Rights Reserved.
+          </p>
+          <div className="flex space-x-4 mt-4 ">
+            <a href="#" className="text-gray-400 hover:text-white">
+              개인정보 처리방침
+            </a>
+            <a href="#" className="text-gray-400 hover:text-white">
+              이용약관
+            </a>
+            <a href="#" className="text-gray-400 hover:text-white">
+              Contact Us
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,104 +5,161 @@ import Link from "next/link";
 import Image from "next/image";
 
 const Navbar = () => {
-    const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
-    const [showNavbar, setShowNavbar] = useState(true);
+  const [hoveredMenu, setHoveredMenu] = useState<string | null>(null);
+  const [showNavbar, setShowNavbar] = useState(true);
 
-    const handleMouseEnter = (menu: string) => {
-        setHoveredMenu(menu);
+  const handleMouseEnter = (menu: string) => {
+    setHoveredMenu(menu);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredMenu(null);
+  };
+
+  useEffect(() => {
+    let lastScrollTop = 0;
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > lastScrollTop) {
+        // 스크롤 다운 시 메뉴바 숨김
+        setShowNavbar(false);
+      } else {
+        // 스크롤 업 시 메뉴바 표시
+        setShowNavbar(true);
+      }
+
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // 스크롤 위치 업데이트
     };
 
-    const handleMouseLeave = () => {
-        setHoveredMenu(null);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    useEffect(() => {
-        let lastScrollTop = 0;
-        const handleScroll = () => {
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return (
+    <nav
+      className={`fixed top-0 left-0 w-full bg-white p-4 flex items-center justify-between z-50 transition-transform duration-300 ${
+        showNavbar ? "transform translate-y-0" : "transform -translate-y-full"
+      }`}
+      style={{
+        height: 70,
+        borderBottomColor: "black",
+        borderBottomStyle: "solid",
+        borderBottomWidth: 1,
+      }}
+    >
+      {/* 왼쪽의 페이지 로고 */}
+      <Link href="/" passHref style={{ position: "fixed", left: 30 }}>
+        <div className="flex items-center cursor-pointer">
+          <Image
+            src="/images/logo2.png" // 로고 이미지 경로
+            alt="Site Logo"
+            width={130} // 로고 이미지 너비
+            height={50} // 로고 이미지 높이
+          />
+        </div>
+      </Link>
 
-            if (scrollTop > lastScrollTop) {
-                // 스크롤 다운 시 메뉴바 숨김
-                setShowNavbar(false);
-            } else {
-                // 스크롤 업 시 메뉴바 표시
-                setShowNavbar(true);
-            }
+      {/* 메뉴 항목들 */}
+      <div className="flex justify-center w-full space-x-12">
+        <Link href="/AIPlatform">
+          <div
+            className="text-black px-4 py-5 cursor-pointer hover:bg-gray-300"
+            style={{ fontWeight: 600, fontSize: 17, borderRadius: 7 }}
+          >
+            AI Platform
+          </div>
+        </Link>
 
-            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // 스크롤 위치 업데이트
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    return (
-        <nav
-            className={`fixed top-0 left-0 w-full bg-gray-800 p-4 flex items-center justify-between z-50 transition-transform duration-300 ${
-                showNavbar ? "transform translate-y-0" : "transform -translate-y-full"
-            }`}
+        <div
+          className="relative text-black px-4 py-5 cursor-pointer hover:bg-gray-300"
+          onMouseEnter={() => handleMouseEnter("Technic")}
+          onMouseLeave={handleMouseLeave}
+          style={{
+            fontWeight: 600,
+            fontSize: 17,
+            borderRadius: 7,
+          }}
         >
-            {/* 왼쪽의 페이지 로고 */}
-            <Link href="/public" passHref>
-                <div className="flex items-center cursor-pointer">
-                    <Image
-                        src="/images/logo.png"  // 로고 이미지 경로
-                        alt="Site Logo"
-                        width={100} // 로고 이미지 너비
-                        height={50} // 로고 이미지 높이
-                    />
-                </div>
-            </Link>
-
-            {/* 메뉴 항목들 */}
-            <div className="flex justify-center w-full space-x-8">
-                <Link href="/AIPlatform">
-                    <div className="text-white px-4 py-2 cursor-pointer hover:bg-gray-700">AI Platform</div>
-                </Link>
-
+          Technic
+          {hoveredMenu === "Technic" && (
+            <div
+              className="absolute top-full left-0 bg-white flex flex-col w-max"
+              style={{
+                left: -126,
+                top: 67,
+                borderColor: "black",
+                borderStyle: "solid",
+                borderWidth: 1,
+                textAlign: "center",
+                // marginTop: 30,
+              }}
+            >
+              <Link href="/Technic/LLM">
                 <div
-                    className="relative text-white px-4 py-2 cursor-pointer hover:bg-gray-700"
-                    onMouseEnter={() => handleMouseEnter("Technic")}
-                    onMouseLeave={handleMouseLeave}
+                  className="text-black px-4 py-2 cursor-pointer hover:bg-gray-300"
+                  style={{ fontWeight: 600, fontSize: 15 }}
                 >
-                    Technic
-                    {hoveredMenu === "Technic" && (
-                        <div className="absolute top-full left-0 bg-gray-700 flex flex-col w-max">
-                            <Link href="/Technic/LLM">
-                                <div className="text-white px-4 py-2 cursor-pointer hover:bg-gray-600">LLM/RAG/Agent</div>
-                            </Link>
-                            <Link href="/Technic/chatbot">
-                                <div className="text-white px-4 py-2 cursor-pointer hover:bg-gray-600">
-                                    챗봇 (지식형-RAG / 분석형-agent / 워크플로우형)
-                                </div>
-                            </Link>
-                        </div>
-                    )}
+                  LLM/RAG/Agent
                 </div>
-
+              </Link>
+              <Link href="/Technic/chatbot">
                 <div
-                    className="relative text-white px-4 py-2 cursor-pointer hover:bg-gray-700"
-                    onMouseEnter={() => handleMouseEnter("Q&A")}
-                    onMouseLeave={handleMouseLeave}
+                  className="text-black px-4 py-2 cursor-pointer hover:bg-gray-300"
+                  style={{ fontWeight: 600, fontSize: 15 }}
                 >
-                    Q&A
-                    {hoveredMenu === "Q&A" && (
-                        <div className="absolute top-full left-0 bg-gray-700 flex flex-col w-max">
-                            <Link href="/QnA/Contact">
-                                <div className="text-white px-4 py-2 cursor-pointer hover:bg-gray-600">제품 문의</div>
-                            </Link>
-                            <Link href="/QnA/Notice">
-                                <div className="text-white px-4 py-2 cursor-pointer hover:bg-gray-600">주요 소식</div>
-                            </Link>
-                        </div>
-                    )}
+                  챗봇 (지식형-RAG / 분석형-agent / 워크플로우형)
                 </div>
+              </Link>
             </div>
-        </nav>
-    );
+          )}
+        </div>
+
+        <div
+          className="relative text-black px-4 py-5 cursor-pointer hover:bg-gray-300"
+          onMouseEnter={() => handleMouseEnter("Q&A")}
+          onMouseLeave={handleMouseLeave}
+          style={{ fontWeight: 600, fontSize: 17, borderRadius: 7 }}
+        >
+          Q&A
+          {hoveredMenu === "Q&A" && (
+            <div
+              className="absolute top-full left-0 bg-white flex flex-col w-max"
+              style={{
+                left: -13,
+                top: 67,
+                borderColor: "black",
+                borderStyle: "solid",
+                borderWidth: 1,
+                textAlign: "center",
+              }}
+            >
+              <Link href="/QnA/Contact">
+                <div
+                  className="text-black px-4 py-2 cursor-pointer hover:bg-gray-300"
+                  style={{ fontWeight: 600, fontSize: 15 }}
+                >
+                  제품 문의
+                </div>
+              </Link>
+              <Link href="/QnA/Notice">
+                <div
+                  className="text-black px-4 py-2 cursor-pointer hover:bg-gray-300"
+                  style={{ fontWeight: 600, fontSize: 15 }}
+                >
+                  주요 소식
+                </div>
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
